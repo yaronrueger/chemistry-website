720f65ac15a22c4332cd6d3faf3a56a0991ccef6
yaron.rueger1 <yaron.rueger@t-systems.com>
2025-10-20 20:57:53 +0200

    remove import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
-import SymposiumBanner from "./SymposiumBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
